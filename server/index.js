979042008f0f0a5d783820d96f317c125ec68708
amiirhussain <amirh.pcr@gmail.com>
2023-08-12 15:28:20 +0500

    first commit

--- server/index.js
+++ server/index.js
@@ -73,30 +73,6 @@ app.post('/api/login', async (req, res) => {
   }
 });
 
-app.post('/api/quote', async (req, res) => {
-	const token = req.headers['x-access-token']
-
-	try {
-		const decoded = jwt.verify(token, 'secret123')
-		const email = decoded.email
-		await User.updateOne(
-			{ email: email },
-			{ $set: { quote: req.body.quote } }
-		)
-
-		return res.json({ status: 'ok' })
-	} catch (error) {
-		console.log(error)
-		res.json({ status: 'error', error: 'invalid token' })
-	}
-})
-
-
-// app.post('/api/logout', (req, res) => {
-//   // You can add code here to invalidate the token or perform any other necessary actions
-//   res.json({ status: 'ok' });
-// });
-
 
 app.listen(1337, () => {
   console.log("Server started on port 1337");
